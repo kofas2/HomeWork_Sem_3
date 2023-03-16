9d2c33c54ac1aa7a39a4fe452489d52327b53748
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-16 18:30:31 +0300

    add Task005 Home work 3

---
+++ Task005/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 19 - HARD необязательная
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, 
+// является ли оно палиндромом. Через строку нельзя решать само собой.
+
+try
+{
+Console.WriteLine("Введите целое число любой разрядности: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int remind = 0;
+int sum = 0;
+int temp = num;
+CheckPaly(num, remind, sum, temp);
+}
+catch
+{
+    Console.WriteLine("Введено некорректное значение");
+}
+
+static void CheckPaly(int num, int remind, int sum, int temp)
+{
+    if (num > 10)
+    {
+        while (num > 0)
+        {
+            remind = num % 10;
+            sum = (sum * 10) + remind;
+            num = num / 10;
+        }
+        if (temp == sum)
+        {
+            Console.WriteLine($"{temp} - является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"{temp} - не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{num} - не является палиндромом");
+    }
+}
